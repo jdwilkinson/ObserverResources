560fe17e2714889e04a285a98a4bdc2a578b5d27
jdwilkinson <jdwilkinson@users.noreply.github.com>
2026-05-25 15:21:18 -0500

    First stab at a list of things to include on a resources site

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,52 @@ const App = () => {
   return (
     <main>
       <section>
-        <h1>React + Vite</h1>
-        <p>Welcome to your new React workspace.</p>
-        <p>Hello, World, etc!</p>
+        <h1>Resources to include</h1>
+        <ul>
+          <li><a href='https://www.spinultimate.com/collections/usa-ultimate-observer-jerseys'>Official jerseys</a></li>
+          <li>Other apparel recommendations
+            <ul>(scour Discord for shorts, jackets, hats, etc)</ul>
+          </li>
+          <li>Scorecards
+            <ul>
+              <li>Official one from the observer manual</li>
+              <li>Zeta's horizontal style</li>
+              <li>Wally's vertical style</li>
+            </ul>
+          </li>
+          <li>Rules quizzes
+            <ul>
+              <li><a href='https://docs.google.com/forms/d/e/1FAIpQLScidc_cjJymmk79dqUQKoIT4XOlqpDhVVwxkM3K7AxdEiYL2g/viewform?usp=send_form'>College Nationals 2026 pre-quiz</a></li>
+              <li>Other rules quizzes from Cotton?</li>
+            </ul>
+          </li>
+          <li>Video replays
+            <ul>
+              <li>...</li>
+            </ul>
+          </li>
+          <li>Observer University videos
+            <ul>
+              <li><a href='https://www.youtube.com/watch?v=jEssYG0SwO8'>Volume 1</a></li>
+              <li><a href='https://www.youtube.com/watch?v=HndetKAiOmE'>Volume 2</a></li>
+              <li><a href='https://www.youtube.com/watch?v=HndetKAiOmE'>Volume 3</a></li>
+            </ul>
+          </li>
+          <li>Official documents
+            <ul>
+              <li>Rules (<a href='https://usaultimate.org/rules/'>Source</a>)</li>
+              <li>Observer manual (<a href='https://usaultimate.org/wp-content/uploads/2020/11/2024-Observer-Manual-2024-04-25.pdf'>Outdated link</a>, <a href='https://usaultimate.org/observers/'>Source</a>)</li>
+            </ul>
+          </li>
+          <li>Apps<ul>
+            <li>SOTA?</li>
+          </ul>
+          </li>
+          <li>Supplemental education<ul>
+            <li><a href='https://www.amazon.com/Social-Ref-Become-Better-Referee/dp/1793133549'>The Social Ref book</a></li>
+          </ul>
+          </li>
+        </ul>
       </section>
     </main>
   );
