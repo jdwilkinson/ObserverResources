2c71b45378ecefb103b25948ce24785263d8b2aa
jdwilkinson <jdwilkinson@users.noreply.github.com>
2026-06-07 21:14:54 -0700

    Refactor scorecard-specific things into the base Resource class.

--- src/types/Resource.tsx
+++ src/types/Resource.tsx
@@ -1,13 +1,89 @@
+export interface ResourceProperties {
+    id: string;
+    image: ResourceImage;
+    description: string;
+    link: string;
+    isOfficialUsauResource?: boolean;
+    courtesyOf?: string;
+}
+
+export type ResourceImage = {
+    sourceUrl: string;
+    width: number;
+    height: number;
+}
+
 abstract class Resource {
     protected id: string;
+    protected image: ResourceImage;
+    protected description: string;
+    protected link: string;
     protected isOfficialUsauResource: boolean;
+    protected courtesyOf: string;
 
-    constructor(id: string, isOfficialUsauResource: boolean) {
+    constructor({
+        id,
+        image,
+        description,
+        link,
+        isOfficialUsauResource = false,
+        courtesyOf = '',
+    }: ResourceProperties) {
         this.id = id;
+        this.image = image;
+        this.description = description;
+        this.link = link;
         this.isOfficialUsauResource = isOfficialUsauResource;
+        this.courtesyOf = courtesyOf;
     }
 
-    abstract render(): JSX.Element;
+    public render(): JSX.Element {
+        return (
+            <div className="resource-card">
+                <a href={this.link} target="_blank" rel="noopener noreferrer">
+                    <img
+                        src={this.image.sourceUrl}
+                        width={this.image.width}
+                        height={this.image.height}
+                        alt={this.getImageAltText()}
+                    />
+                </a>
+                <p>
+                    {this.description}
+                    {this.getPostDescriptionFooter()}
+                </p>
+            </div>
+        );
+    }
+
+    private getImageAltText(): string {
+        if (this.isOfficialUsauResource) {
+            return 'Official USAU resource';
+        }
+        if (this.courtesyOf.trim() !== '') {
+            return `Resource by ${this.courtesyOf}`;
+        }
+        return '';
+    }
+
+    private getPostDescriptionFooter(): JSX.Element {
+        if (this.isOfficialUsauResource) {
+            return (<>
+                <br />
+                <img src='/img/usau-logo.svg' alt='Official USAU resource' width='80' height='38' />
+            </>);
+        }
+        if (this.courtesyOf && this.courtesyOf.trim() !== '') {
+            return (<>
+                <br />
+                <small>
+                    <em>Courtesy of <b>{this.courtesyOf}</b>
+                    </em>
+                </small>
+            </>);
+        }
+        return (<></>);
+    }
 }
 
 export default Resource;
