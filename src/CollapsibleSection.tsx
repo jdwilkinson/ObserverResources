0b3efc6bc73e85a34ceea98897890e434a113b7f
jdwilkinson <jdwilkinson@users.noreply.github.com>
2026-06-13 12:50:34 -0700

    Add count of elements to each collapsible section.

--- src/CollapsibleSection.tsx
+++ src/CollapsibleSection.tsx
@@ -18,10 +18,24 @@ const CollapsibleSection = (props: { sectionTitle: string; children: React.React
         return "click";
     }
 
+    function getHeaderCountString(): JSX.Element {
+        if (!props.children) {
+            return <></>;
+        }
+        if (!collapsed) {
+            return <></>;
+        }
+        const count = Array.isArray(props.children) ? props.children.length : '?';
+        return <span className="grayscale"> &nbsp; ({count})</span>;
+    }
+
     return (
         <>
             <div className="section-header" onClick={onClick}>
-                <span>{props.sectionTitle}</span>
+                <span>
+                    <span>{props.sectionTitle}</span>
+                    {getHeaderCountString()}
+                </span>
                 <span className="click-to-collapse">({getToggleVerb()} to {collapsed ? "expand" : "collapse"})</span>
             </div>
             {collapsed ?
